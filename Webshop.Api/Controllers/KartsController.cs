3ab8391b8fdf3b94bc8336073c9f7846181984be
Applemelon <32298607+Applemelon@users.noreply.github.com>
2018-10-08 11:35:24 +0200

    Added other controllers

---
+++ Webshop.Api/Controllers/KartsController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Group13.Webshop.Core.Entity;
+using Group13.Webshop.Core.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Webshop.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KartsController : ControllerBase
+    {
+        private IKartService _KartService;
+
+        public KartsController(IKartService kartService)
+        {
+            _KartService = kartService;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public ActionResult<IEnumerable<Kart>> Get()
+        {
+            return null;
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}", Name = "Get")]
+        public ActionResult<Kart> Get(int id)
+        {
+            return null;
+        }
+
+        // POST: api/Products
+        [HttpPost]
+        public ActionResult<Kart> Post([FromBody] Kart value)
+        {
+            return null;
+        }
+
+        // PUT: api/Products/5
+        [HttpPut("{id}")]
+        public ActionResult<Kart> Put(int id, [FromBody] Kart value)
+        {
+            return null;
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public ActionResult<Kart> Delete(int id)
+        {
+            return null;
+        }
+    }
+}
---
+++ Webshop.Api/Controllers/UsersController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Group13.Webshop.Core.Entity;
+using Group13.Webshop.Core.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Webshop.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private IUserService _UserService;
+
+        public UsersController(IUserService userService)
+        {
+            _UserService = userService;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> Get()
+        {
+            return null;
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}", Name = "Get")]
+        public ActionResult<User> Get(int id)
+        {
+            return null;
+        }
+
+        // POST: api/Products
+        [HttpPost]
+        public ActionResult<User> Post([FromBody] User value)
+        {
+            return null;
+        }
+
+        // PUT: api/Products/5
+        [HttpPut("{id}")]
+        public ActionResult<User> Put(int id, [FromBody] User value)
+        {
+            return null;
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public ActionResult<User> Delete(int id)
+        {
+            return null;
+        }
+    }
+}
