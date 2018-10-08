2f9356dc83d1dcf821ec6ec8b3eb009f2d51fc38
Oliver Sylvest Hansen <oliv9858@easv365.dk>
2018-10-08 13:56:00 +0200

    Product Service

--- Group13.Webshop.Core/Service/IProductService.cs
+++ Group13.Webshop.Core/Service/IProductService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Group13.Webshop.Core.Entity;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,10 @@ namespace Group13.Webshop.Core.Service
 {
     public interface IProductService
     {
+        Product Create(Product product);
+
+        void Delete(int id);
+
+        Product ReadById(int id);
     }
 }
--- Group13.Webshop.Core/Service/impl/ProductService.cs
+++ Group13.Webshop.Core/Service/impl/ProductService.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Group13.Webshop.Core.Entity;
+using Group13.Webshop.Core.Repository;
 
 namespace Group13.Webshop.Core.Service.impl
 {
-    public class ProductService: IProductService
+    public class ProductService : IProductService
     {
+        readonly IProductRepository _ProductRepo;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _ProductRepo = productRepository;
+        }
+
+        public Product Create(Product product)
+        {
+            return _ProductRepo.CreateProduct(product);
+        }
+
+        public void Delete(int id)
+        {
+            _ProductRepo.DeleteProduct(id);
+        }
+
+        public Product ReadById(int id)
+        {
+            return _ProductRepo.ReadById(id);
+        }
     }
 }
